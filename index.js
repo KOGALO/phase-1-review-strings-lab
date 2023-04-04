8b513f8190b5a6bd8b4078245dfea02fe3c81afd
Kepher <kepher.ogalo@student.moringaschool.com>
2023-04-04 02:01:02 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+// 
+const currentUser = 'Kepher Ogalo';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
+
